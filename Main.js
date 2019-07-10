d8ff5e2980e68d97e3a536340c3c6cb6beb6cfbc
joyce <joycerjtz@gmail.com>
2019-07-09 23:42:23 -0300

    função produção unitaria e chomsky

--- Main.js
+++ Main.js
@@ -18,7 +18,7 @@
  */
 
 let entrada, saida;
-let G, C;
+let C;
 
 function setup() {
 	entrada = document.getElementById('nde');
@@ -29,4 +29,115 @@ function traduzir() {
 	G = JSON.parse(entrada.value);
 	C = G;
 	saida.value = JSON.stringify(C, null, "\t");
+}
+
+let G = {
+	"variaveis": [
+		"A",
+		"B",
+		"C",
+		"D"
+	],
+	"terminais": [
+		"a",
+		"b",
+		"c"
+	],
+	"inicial": "A",
+	"regras": [
+		{
+			"esq": "A",
+			"dir": "ca"
+		},
+		{
+			"esq": "B",
+			"dir": "b"
+		},
+		{
+			"esq": "C",
+			"dir": "ABD"
+		},
+		{
+			"esq": "D",
+			"dir": "BBD"
+		}
+	]
+}
+
+function producao_unitaria() {
+	var variaveis = new Set(G.variaveis);
+	var aux = 1, k;
+	while (aux) {
+		aux = 0; k = 0;
+		for (let i of G.regras) {
+			if (i.dir.length == 1 && variaveis.has(i.dir)) {
+				aux = 1;
+				for (let j of G.regras) {
+					if (j.esq == i.dir) {
+						G.regras.push({ "esq": i.esq, "dir": j.dir });
+					}
+				}
+				G.regras.splice(k, 1);
+			} k++;
+		}
+	}
+}
+
+var geradores_var = ['α', ' β', 'γ', 'δ', 'ζ', 'η', 'θ', 'Ϸ', 'λ', 'Σ', 'Ω', 'Δ', 'Λ', 'μ', 'π', 'σ', 'φ', 'ψ', 'ω', 'א', 'ב', 'ג', 'ש', '该', '大', '号', 'и', 'Я'];
+
+function ch() {
+	troca_terminais();
+	tamanho2();
+}
+
+function troca_terminais() {
+	var terminais = new Set(G.terminais);
+	for (let i of G.regras) {
+		if (i.dir.length > 1) {
+			for (let j = 0; j < i.dir.length; j++) {
+				if (terminais.has(i.dir[j])) {
+					var existe = verifica(i.dir[j]);
+					if (existe) {
+						i.dir = i.dir.replace(i.dir[j], existe.esq);
+					}
+					else {
+						var vari = geradores_var.pop();
+						G.regras.push({ "esq": vari, "dir": i.dir[j] });
+						G.variaveis.push(vari);
+						i.dir = i.dir.replace(i.dir[j], vari);
+					}
+				}
+			}
+		}
+
+	}
+}
+
+function verifica(aux) {
+	for (let i of G.regras) {
+		if (i.dir === aux) return i;
+	}
+	return 0;
+}
+
+function tamanho2() {
+	var aux = 1;
+	while (aux) {
+		aux = 0;
+		for (let i of G.regras) {
+			if (i.dir.length > 2) {
+				aux = 1;
+				var existe = verifica(i.dir.slice(1));
+				if (existe) {
+					i.dir = i.dir.replace(i.dir.slice(1), existe.esq);
+				}
+				else {
+					var vari = geradores_var.pop();
+					G.regras.push({ "esq": vari, "dir": i.dir.slice(1) });
+					i.dir = i.dir.replace(i.dir.slice(1), vari);
+					G.variaveis.push(vari);
+				}
+			}
+		}
+	}
 }
